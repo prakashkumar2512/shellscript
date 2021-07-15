1bcd4c11eac3473373dc9030c7f83105e0f1f07b
prakash <prakashverma@gmail.com>
2021-07-15 02:44:23 +0000

    added git ignore file

--- mysqlDBbackup.sh
+++ mysqlDBbackup.sh
@@ -1,3 +1,8 @@
 #!/bin/bash #Shebang
 
-mysqldump -h db_host.rds.aws.com -u dbuser -p welcome1 db_flipkart > /db-backup/db_flipkart.sql
+v_dbName=$1 
+v_userName=$(cat cred | grep username | awk -F"=" '{print $2}')
+v_password=$(cat cred | grep password | awk -F"=" '{print $2}')
+v_dbHost=db_host.rds.aws.com
+
+echo "mysqldump -h $v_dbHost  -u $v_userName -p $v_password $v_dbName > /db-backup/$v_dbName.sql"
---
+++ readme.txt
@@ -0,0 +1,8 @@
+1. step to run mysqlDBbackup.sh
+	- Create a "cred" file in local folder from where you want to run script.
+		Ex -
+			cat cred
+			username=dbuser
+			password=welcome1
+2. Run the script with DB name as Argument to it.
+	- sh.mysqlDBbackup.sh db_sanpdeal
